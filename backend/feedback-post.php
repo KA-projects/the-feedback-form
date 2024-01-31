168d71d2d7cbac75921e788a5ebcbb4acc73860d
KA-projects <122209044+KA-projects@users.noreply.github.com>
2024-02-01 00:36:58 +0600

    added functionality do post and fix sql query

--- backend/feedback-post.php
+++ backend/feedback-post.php
@@ -5,29 +5,47 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $text = $_POST["text"];
+$user = "admin";
+$password = "password";
+$database = "feedbacks";
+$table = "feedbacks.feedbacks_list";
 
-    $response = [
-        'status' => 'success',
-        'message' => 'Data received successfully',
-        'data' => [
-            'name' => $name,
-            'email' => $email,
-            'text' => $text,
-        ],
-    ];
+try {
+    $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
 
-    echo json_encode($response);
-} else {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $text = $_POST["text"];
+        $date = $_POST["date"];
+
+        $stmt = $db->prepare("INSERT INTO $table (name, email, text, date) VALUES (:name, :email, :text, :date)");
+
+        $stmt->bindParam(':name', $name);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':text', $text);
+        $stmt->bindParam(':date', $date);
+
+        $stmt->execute();
+
+        $count = $stmt->rowCount();
+
+        echo json_encode($count);
+    } else {
+        $response = [
+            'status' => 'error',
+            'message' => 'Invalid request method',
+        ];
+        echo json_encode($response);
+    }
+} catch (PDOException $e) {
     $response = [
         'status' => 'error',
-        'message' => 'Invalid request method',
+        'message' => $e->getMessage(),
     ];
-
     echo json_encode($response);
 }
+
+
 
 ?>
